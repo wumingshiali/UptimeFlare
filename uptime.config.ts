5875992a209030d3630b23a1cfbc4a5549df9e35
VoidCat <ZWj1154142014@hotmail.com>
2026-09-11 20:33:00 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -81,7 +81,16 @@ const workerConfig: WorkerConfig = {
 
 // const maintenances: MaintenanceConfig[] = []
 
-const maintenances: MaintenanceConfig[] = []
+const maintenances: MaintenanceConfig[] = [
+  {
+    monitor: ["umami"],
+    title: "Umami 严重事故",
+    body: "此次事故的原因是尝试给使用 Edgeone 部署的 Umami 添加 Cloudflare 代理，由于两边都配置了 https 重定向导致无限重定向",
+    start: "2026-09-11T17:04:00+08:00",
+    end: "2026-09-11T19:58:00+08:00",
+    color: "red"
+  }
+]
 
 // Don't edit this line
 export { maintenances, pageConfig, workerConfig }
